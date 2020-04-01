babd0909803b99f2fa924f61cc8cf9bf042b5e6f
svasisht <vasisht.shankar@gmail.com>
2020-04-01 16:52:26 -0700

    modified search parameter to icao code

--- README.md
+++ README.md
@@ -8,7 +8,5 @@ Get historical monthly weather data for any city in the world in json format. Th
 2. The program will prompt you for the following inputs:
     1. start date in mm-yyyy format.
     2. end date in mm-yyyy format.
-    3. country code.
-    4. state code.
-    5. city name. If name contains spaces, add a '-' in between.
+    3. icao code for airport. icao code for your city or your neighboring city's airport can be found at: https://www.world-airport-codes.com/.
 3. The output json file containing weather data will be saved in the same directory. 
--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const fs = require('fs');
 
 prompt.start();
 
-prompt.get(['start [mm-yyyy]', 'end [mm-yyyy]', 'country', 'state_abbreviation', 'city'], async function (err, result) {
+prompt.get(['start [mm-yyyy]', 'end [mm-yyyy]', 'icao_code'], async function (err, result) {
     if (err) { return onErr(err); }
-    let [startDate, endDate, country, state, city] = [
+    let [startDate, endDate, icaoCode] = [
         moment(`01-${result['start [mm-yyyy]']}`, 'DD-MM-YYYY'), 
         moment(`31-${result['end [mm-yyyy]']}`, 'DD-MM-YYYY'),
-        result['country'],
-        result['state_abbreviation'],
-        result['city']
+        result['icao_code']
     ];
+    console.log('Scraping the web...')
     let weatherData = {};
     for (var m = moment(startDate); m.isBefore(endDate); m.add(1, 'months')) {
         let date = m.format('YYYY-MM');
-        let weatherUrl = `https://www.wunderground.com/history/monthly/${country}/${state}/${city}/date/${date}`;
+        let weatherUrl = `https://www.wunderground.com/history/monthly/${icaoCode}/date/${date}`;
+        console.log(`Getting data for date ${date} from ${weatherUrl}`);
         weatherData[date] = await scraper.scrapeWunderGround(date, weatherUrl);
     }
-    const fileName = `./${city}-${result['start [mm-yyyy]']}-${result['end [mm-yyyy]']}.json`;
+    const fileName = `./data/${icaoCode}-${result['start [mm-yyyy]']}-${result['end [mm-yyyy]']}.json`;
     fs.writeFileSync(fileName, JSON.stringify(weatherData, null, 4));
-    console.log(`output saved to ${fileName}`);
+    console.log(`Output saved to ${fileName}`);
 });
 
 function onErr(err) {
